44851565d0c721b60abf53bcfade7646cd2f2dce
Alberto <tejotaesi@gmail.com>
2022-06-12 11:32:21 -0300

    feat: Creation of a field createAt in the tables to store the date of creation of the entities

--- database/script/tables.sql
+++ database/script/tables.sql
@@ -2,13 +2,15 @@ CREATE TABLE users (
     id serial PRIMARY KEY,
     name text NOT NULL,
     email text NOT NULL UNIQUE,
-    password text NOT NULL
+    password text NOT NULL,
+    "createdAt" TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE sessions (
     id serial PRIMARY KEY,
     "userId" integer REFERENCES users(id),
-    token text NOT NULL
+    token text NOT NULL,
+    "createdAt" TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE urls (
@@ -16,7 +18,8 @@ CREATE TABLE urls (
     "userId" integer NOT NULL REFERENCES users(id),
     url text NOT NULL,
     "shortUrl" text NOT NULL UNIQUE,
-    "visitCount" integer NOT NULL DEFAULT 0
+    "visitCount" integer NOT NULL DEFAULT 0,
+    "createdAt" TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW()
 );
 
 
